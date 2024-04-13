64063c2e747688825fdd6c1569f54a6f3a8d4f12
Scott Quinton <scott@quinton.tech>
2024-04-13 04:07:13 -0600

    Added VS Solution

---
+++ ExtensionClassTestingFramework/Program.cs
@@ -0,0 +1,78 @@
+﻿using Mongoose.IDO;
+using Mongoose.IDO.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExtensionClassTestingFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string slserver = "https://YourSyteLineServer/IDORequestService/RequestService.aspx";
+
+            Client oclient = new Client(slserver, IDOProtocol.Http);
+            DataTable dtResults = new DataTable();
+
+            try
+            {
+                OpenSessionResponseData res = oclient.OpenSession("username@domain.com", "ThisIsAPassword", "ConfigurationName");
+
+                if (res.Result.ToString() != "Success") { throw new Exception(res.Result.ToString()); }
+
+                dtResults = YourExtensionClassCode.Process_MyMethod(context: oclient, Param1: "Value", Param2: "Value");
+
+                Console.WriteLine(dtResults.Rows.Count);
+
+                string curr = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+                output_results(dtResults, "C:\\output_" + curr + ".csv");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+            finally
+            {
+                oclient.CloseSession();
+                Console.Read();
+            }
+
+        }
+
+
+        static void output_results(DataTable data, string path)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (DataColumn col in data.Columns)
+            {
+                sb.Append(col.ColumnName + ',');
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append(Environment.NewLine);
+
+            foreach (DataRow row in data.Rows)
+            {
+                foreach (DataColumn col in data.Columns)
+                {
+                    sb.Append(row[col].ToString().Replace(",", "|") + ",");
+                }
+
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append(Environment.NewLine);
+            }
+
+            StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8);
+
+            sw.WriteLine(sb);
+            sw.Close();
+        }
+    }
+}
---
+++ ExtensionClassTestingFramework/YourExtensionClassCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+using Mongoose.Core.Common;
+using Mongoose.Core.DataAccess;
+using Mongoose.IDO;
+using Mongoose.IDO.Protocol;
+
+namespace ExtensionClassTestingFramework
+{
+    // [IDOExtensionClass( "YourExtensionClassCode" )]
+    public partial class YourExtensionClassCode : IDOExtensionClass
+    {
+        [IDOMethod(MethodFlags.CustomLoad, "Infobar")]
+        public DataTable MyMethod(string Param1, string Param2)
+        {
+            return Process_MyMethod(this.Context.Commands, Param1, Param2);
+        }
+        public static DataTable Process_MyMethod(IIDOCommands context, string Param1, string Param2)
+        {
+            DataTable dtResults = new DataTable();
+            LoadCollectionRequestData req = new LoadCollectionRequestData();
+            LoadCollectionResponseData rsp = new LoadCollectionResponseData();
+
+            req.IDOName = "SLItems";
+            req.Filter = "PMTCode = 'P'";
+            req.RecordCap = 1000;
+
+            rsp = context.LoadCollection(req);
+            rsp.Fill(dtResults);
+
+            return dtResults;
+        }
+    }
+}
